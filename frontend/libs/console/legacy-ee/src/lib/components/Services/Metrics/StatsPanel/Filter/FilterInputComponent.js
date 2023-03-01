01d79867eb25004bc659783a40b2b75f8d6e35f8
robertjdominguez <rob.dominguez@hasura.io>
2023-03-01 08:29:23 -0600

    2.20 release deployment

--- frontend/libs/console/legacy-ee/src/lib/components/Services/Metrics/StatsPanel/Filter/FilterInputComponent.js
+++ frontend/libs/console/legacy-ee/src/lib/components/Services/Metrics/StatsPanel/Filter/FilterInputComponent.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import clsx from 'clsx';
 
 /* calls the onChange configured when enter is pressed
  * and clears the current input
@@ -20,14 +19,10 @@ const FilterInputComponent = props => {
     }
   };
   return (
-    <div className={clsx(styles.selectBox)} id={id}>
+    <div className={styles.selectBox} id={id}>
       <span>{filterTitle}</span>
       <input
-        className={clsx(
-          styles.commonInput,
-          'w-full',
-          'block font-normal w-full h-input shadow-sm rounded border border-gray-300 hover:border-gray-400 focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-yellow-200 focus-visible:border-yellow-400 placeholder-gray-500'
-        )}
+        className={styles.commonInput}
         type="text"
         placeholder={placeholder}
         onKeyDown={handleEnterKeyPress}
